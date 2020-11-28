563c4d66ff79227096752502e594031d61b07a00
shridhargiri <61384882+shridhargiri@users.noreply.github.com>
2020-11-28 18:22:43 +0530

    POCO Class for EmployeeProgram

---
+++ Practice/EmployeeApp_ADO/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ques1_Employee
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
